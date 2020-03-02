b08e1fd17e4ea8531272c49bbbd3438d190d56b8
Alexander Goldberg <38928216+vlexvnder@users.noreply.github.com>
2020-03-02 14:23:08 -0800

    Update score route
    Changed from using "get" method to "post" method

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from Analyze import score as score_text
 from base64 import b64decode as b64d
 app = Flask(__name__)
+
+app.secret_key = b'_5#y2gyhsghyttryrthgfjkjutrtqtregdfgdL"F4Q8z\n\asdasdasdas]/'
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/score/<text>')
-def score(text):
-    avg, good, bad = score_text(text)
-    return jsonify({
-        'score': avg,
-        'good': good,
-        'bad': bad
-    })
+@app.route('/score', methods=["GET","POST"])
+def score():
+    if request.method == "POST":
 
-app.run(debug=True)
+        content = request.form['content']
+        avg, good, bad = score_text(content)
+
+        return jsonify({
+            'score': avg,
+            'good': good,
+            'bad': bad
+        })
+    return False
+
